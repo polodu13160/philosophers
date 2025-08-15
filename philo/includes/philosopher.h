f1d3590558affea70d80694e58b0263035c0b7bf
paul de petrini <paul.depetrini@gmail.com>
2025-08-15 18:58:29 +0000

    pthread creates and genere first messages for one philosophers test

--- philo/includes/philosopher.h
+++ philo/includes/philosopher.h
@@ -2,6 +2,9 @@
 # define H_PHILOSOPHER
 
 # include <pthread.h>
+#include <sys/time.h>
+
+
 
 typedef enum e_action
 {
@@ -9,22 +12,26 @@ typedef enum e_action
 	EAT,
 	SLEEP,
 	THINK,
-}				t_action;
+}					t_action;
 
 typedef struct s_pnj
+
 {
-	pthread_t	value_thread;
-	int			*philo_number;
-	int			action;
-}				t_pnj;
+	pthread_t		value_thread;
+	int				philo_number;
+	int				action;
+	struct s_pnj	*next;
+}					t_pnj;
+
 typedef struct s_philo
 {
-	long int	number_of_philosophers;
-	long int	time_to_die;
-	long int	time_to_eat;
-	long int	time_to_sleep;
-	long int	number_of_times_eahc_philosopher_must_eat;
-
-	t_pnj		*philosophers;
-}				t_philo;
+	long int		number_of_philosophers;
+	long int		time_to_die;
+	long int		time_to_eat;
+	long int		time_to_sleep;
+	long int		number_of_times_eahc_philosopher_must_eat;
+	struct timeval	time_start;
+	t_pnj			*philo_uno;
+	t_pnj			*philosophers;
+}					t_philo;
 #endif
--- philo/sources/main.c
+++ philo/sources/main.c
@@ -3,15 +3,17 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: pde-petr <pde-petr@student.42.fr>          +#+  +:+       +#+        */
+/*   By: pdepetrini <pdepetrini@student.42.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/22 02:39:39 by pde-petr          #+#    #+#             */
-/*   Updated: 2025/07/22 22:41:50 by pde-petr         ###   ########.fr       */
+/*   Updated: 2025/08/15 18:55:08 by pdepetrini       ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philosopher.h"
+#include "stdio.h"
 #include "stdlib.h"
+#include <errno.h>
 #include <limits.h>
 #include <sys/time.h>
 #include <unistd.h>
@@ -28,6 +30,40 @@ void	ft_message(int action, time_t time, int philo_number)
 		printf("%lu %d is thinking\n", time, philo_number);
 }
 
+int	error_erno(char *message)
+{
+	perror(message);
+	return (errno);
+}
+
+long int	calc_time(int time_mili_start)
+{
+	long int		time_mili;
+	struct timeval	time_now;
+
+	if (gettimeofday(&time_now, NULL) <= -1)
+	{
+		return (-1);
+	}
+	time_mili = (time_now.tv_sec * 1000 + time_now.tv_usec / 1000)
+		- time_mili_start;
+	return (time_mili);
+}
+
+void	*to_be_philosopher(void *philo)
+{
+	long int	time_mili_start;
+	int			time_now_in_mili;
+
+	time_mili_start = (((t_philo *)philo)->time_start.tv_sec * 1000
+			+ ((t_philo *)philo)->time_start.tv_usec / 1000);
+	time_now_in_mili = calc_time(time_mili_start);
+	if (time_now_in_mili <= -1)
+		error_erno("error Time in thread");
+	ft_message(FORK, time_now_in_mili,
+		((t_philo *)philo)->philosophers->philo_number);
+	return (NULL);
+}
 long int	ft_atol_no_neg(char *argv)
 {
 	int		neg;
@@ -76,19 +112,68 @@ int	parsing_argv(char **argv, t_philo *list, int argc)
 	return (0);
 }
 
+void	free_philo(t_pnj *philo)
+{
+	t_pnj	*temp;
+
+	while (philo->next != NULL)
+	{
+		temp = philo->next;
+		free(philo);
+		philo = temp;
+	}
+	free(philo);
+}
+
 int	main(int argc, char **argv)
 {
 	t_philo list;
 	list.number_of_philosophers = 0;
+	list.philosophers = NULL;
 	list.number_of_times_eahc_philosopher_must_eat = -1;
 	list.time_to_die = 0;
 	list.time_to_eat = 0;
 	list.time_to_sleep = 0;
+	if (gettimeofday(&list.time_start, NULL) == -1)
+		return (error_erno("error of the time"));
+	int i;
+	i = 0;
 	if (parsing_argv(++argv, &list, argc - 1) > 0)
 	{
 		printf("error parsing for overflow or negatif value or too much parameters\n");
 		return (1);
 	}
+	if (list.number_of_philosophers == 0)
+	{
+		printf("are you serious ?\n");
+		return (1);
+	}
+	list.philosophers = malloc(sizeof(t_philo) * 1);
+	if (list.philosophers == NULL)
+		return (1);
+
+	while (i < list.number_of_philosophers)
+	{
+		list.philosophers->next = NULL;
+		if (i == 0)
+			list.philo_uno = list.philosophers;
+		list.philosophers->philo_number = i + 1;
+		pthread_create(&list.philosophers->value_thread, NULL,
+			to_be_philosopher, &list);
+		if (i + 1 != list.number_of_philosophers)
+		{
+			list.philosophers->next = malloc(sizeof(t_philo) * 1);
+			if (list.philosophers->next == NULL)
+			{
+				free_philo(list.philo_uno);
+				return (1);
+			}
+			list.philosophers = list.philosophers->next;
+		}
+		i++;
+	}
+	pthread_join(list.philosophers->value_thread, NULL);
+	free_philo(list.philo_uno);
 
 	return (0);
 }
